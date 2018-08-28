7a8c721f94a6245cb92bc761f590db1fe77365db
josephraguilar <josephraguilar@gmail.com>
2018-08-28 12:53:35 -0700

    initial commit, minor start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MonthComponent } from './Month/month.component';
+import { WeekComponent } from './week/week.component';
+import { DayComponent } from './day/day.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+  path:'Month',
+  component: MonthComponent
+},
+{
+  path: 'Week',
+  component: WeekComponent
+},
+{
+  path: '',
+  component: DayComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { DayComponent } from './day/day.component';
+import { WeekComponent } from './week/week.component';
+import { MonthComponent } from './month/month.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DayComponent,
+    WeekComponent,
+    MonthComponent
   ],
   imports: [
     BrowserModule,
